bcbe7d6c469cd600b6e9c296204aee0e01ebbf6a
hjz2016 <1073560118@qq.com>
2022-04-16 14:12:27 +0800

    ybb

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import i18n from '@/assets/script/i18n'
 import router from '@/router'
+// 引入调试
+import VConsole from 'vconsole'
 import '@/assets/style/transition.styl'
 import '@/assets/style/animation.styl'
 
+// 非正式环境中启用 vconsole
+const vconsole = new VConsole()
 createApp(App).use(router).use(i18n).mount('#app')
