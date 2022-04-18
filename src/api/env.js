8cde5365b00c9099dccf967414c97d3cc2d2dbe0
hjz2016 <1073560118@qq.com>
2022-04-18 13:27:02 +0800

    env

--- src/api/env.js
+++ src/api/env.js
@@ -2,14 +2,14 @@ import constant from "@/public/constant";
 
 const env = {
     dev: {
-        auth: 'http://www.necora-nyaru.site:8888/tongji/',  //获取网站概况
+        auth: 'https://www.necora-nyaru.site/tongji/',  //获取网站概况
     },
     qas: {
-        auth: 'http://www.necora-nyaru.site:8888/tongji/',  //获取网站概况
+        auth: 'https://www.necora-nyaru.site/tongji/',  //获取网站概况
         
     },
     app: {
-        auth: 'http://www.necora-nyaru.site:8888/tongji/',  //获取网站概况
+        auth: 'https://www.necora-nyaru.site/tongji/',  //获取网站概况
         
     }
 };
