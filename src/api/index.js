5c4798800bafa5d6ce71cfae7b04c0575bb99c9c
hjz2016 <1073560118@qq.com>
2022-04-18 17:40:41 +0800

    2

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import {
 } from './req';
 
 export default {
-  getData: (data) => req('auth', 'GET', 'report/getData', data), //获取网站概况
+  getData: (data) => req('auth', 'GET', 'report/getData', data, 1, null, 1), //获取网站概况
   // getToken: (data) => req('auth', 'get', 'account/register', data, 1, null, 1), //获取登录token
   // login: (data) => req('auth', 'post', 'account/login', data, null, null, 1), //登录新接口
 }
