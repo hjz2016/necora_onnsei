538cfd4166aa2e63bda63cbb1db213ea3d2ace97
hjz2016 <1073560118@qq.com>
2022-04-18 13:18:50 +0800

    +tongji

--- src/api/env.js
+++ src/api/env.js
@@ -2,14 +2,14 @@ import constant from "@/public/constant";
 
 const env = {
     dev: {
-        auth: 'https://openapi.baidu.com/rest/2.0/tongji/',  //获取网站概况
+        auth: 'http://www.necora-nyaru.site:8888/tongji/',  //获取网站概况
     },
     qas: {
-        auth: 'https://openapi.baidu.com/rest/2.0/tongji/',  //获取网站概况
+        auth: 'http://www.necora-nyaru.site:8888/tongji/',  //获取网站概况
         
     },
     app: {
-        auth: 'https://openapi.baidu.com/rest/2.0/tongji/',  //获取网站概况
+        auth: 'http://www.necora-nyaru.site:8888/tongji/',  //获取网站概况
         
     }
 };
