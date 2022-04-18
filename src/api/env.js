f77b2fd4cd48b3b6530cd6432f2ecca8f7228f6a
hjz2016 <1073560118@qq.com>
2022-04-18 18:01:00 +0800

    11

--- src/api/env.js
+++ src/api/env.js
@@ -2,14 +2,14 @@ import constant from "@/public/constant";
 
 const env = {
     dev: {
-        auth_getData: 'https://www.necora-nyaru.site/tongji/',  //获取网站概况
+        auth: 'https://www.necora-nyaru.site/tongji/',  //获取网站概况
     },
     qas: {
-        auth_getData: 'https://www.necora-nyaru.site/tongji/',  //获取网站概况
+        auth: 'https://www.necora-nyaru.site/tongji/',  //获取网站概况
         
     },
     app: {
-        auth_getData: 'https://www.necora-nyaru.site/tongji/',  //获取网站概况
+        auth: 'https://www.necora-nyaru.site/tongji/',  //获取网站概况
         
     }
 };
